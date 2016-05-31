89e39faf1fa856a90a6e5d05b5128662d8b71f51
Mathis Csisinko <csisinko@ulmon.com>
2016-05-31 17:59:47 +0200

    [core] robustness against database connection failures applied
    (cherry picked from commit ab7470278fee5f75f1c000dc04298189e4a7d9f9)

--- platform/default/src/mbgl/storage/default_file_source.cpp
+++ platform/default/src/mbgl/storage/default_file_source.cpp
@@ -131,7 +131,11 @@ public:
         } else {
             // Try the offline database
             if (resource.hasLoadingMethod(Resource::LoadingMethod::Cache)) {
-                auto offlineResponse = offlineDatabase->get(resource);
+                optional<Response> offlineResponse;
+                try {
+                    offlineResponse = offlineDatabase->get(resource);
+                } catch (...) {
+                }
 
                 if (! offlineResponse) {
                     auto supplementaryCachePathsOfKind = supplementaryCachePaths.find(resource.kind);
@@ -143,10 +147,16 @@ public:
                             const auto &cachePath = j->second;
                             auto supplementaryOfflineDatabase = supplementaryOfflineDatabases.find(cachePath);
                             if (supplementaryOfflineDatabase == supplementaryOfflineDatabases.end()) {
-                                supplementaryOfflineDatabase = supplementaryOfflineDatabases.emplace(cachePath, std::make_unique<OfflineDatabase>(cachePath)).first;
+                                try {
+                                    supplementaryOfflineDatabase = supplementaryOfflineDatabases.emplace(cachePath, std::make_unique<OfflineDatabase>(cachePath)).first;
+                                } catch (...) {
+                                }
                             }
                             if (supplementaryOfflineDatabase != supplementaryOfflineDatabases.end()) {
-                                offlineResponse = supplementaryOfflineDatabase->second->get(resource);
+                                try {
+                                    offlineResponse = supplementaryOfflineDatabase->second->get(resource);
+                                } catch (...) {
+                                }
                             }
                         }
                     }
@@ -186,7 +196,10 @@ public:
             if (resource.hasLoadingMethod(Resource::LoadingMethod::Network)) {
                 MBGL_TIMING_START(watch);
                 tasks[req] = onlineFileSource.request(resource, [=] (Response onlineResponse) {
-                    this->offlineDatabase->put(resource, onlineResponse);
+                    try {
+                        this->offlineDatabase->put(resource, onlineResponse);
+                    } catch (...) {
+                    }
                     if (resource.kind == Resource::Kind::Tile) {
                         // onlineResponse.data will be null if data not modified
                         MBGL_TIMING_FINISH(watch,
@@ -240,7 +253,10 @@ public:
     }
 
     void put(const Resource& resource, const Response& response) {
-        offlineDatabase->put(resource, response);
+        try {
+            offlineDatabase->put(resource, response);
+        } catch (...) {
+        }
     }
 
     void resetDatabase(std::function<void (std::exception_ptr)> callback) {
