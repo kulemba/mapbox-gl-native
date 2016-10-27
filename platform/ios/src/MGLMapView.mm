921fde1cde641d97ba92fcd5ad1b55aaad5c0446
Tim <tim@ulmon.com>
2016-10-27 12:32:00 +0200

    [iOS] corrected issue with heading indicator no longer updating for user location annotation after entering user location mode where standard mapbox usually deactivates heading mode.

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -3967,8 +3967,6 @@ public:
         {
             self.userTrackingState = MGLUserTrackingStatePossible;
 
-            [self.locationManager stopUpdatingHeading];
-
             // Immediately update the annotation view; other cases update inside
             // the locationManager:didUpdateLocations: method.
             [self updateUserLocationAnnotationView];
