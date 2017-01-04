4019d59b1c3114bcea8bd42dd571863da1922fac
Mathis Csisinko <csisinko@ulmon.com>
2017-01-04 16:11:31 +0100

    [ios] improved delegate method provided to allow responding in a different way when a user taps and did not select an annotation

--- platform/ios/src/MGLMapView.mm
+++ platform/ios/src/MGLMapView.mm
@@ -1713,6 +1713,12 @@ public:
             }
         }
         [self deselectAnnotation:self.selectedAnnotation animated:YES];
+        if ([self.delegate respondsToSelector:@selector(mapViewTapDidNotSelectAnnotation:)])
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.delegate mapViewTapDidNotSelectAnnotation:self];
+            });
+        }
         UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, nextElement);
 
         return;
@@ -1728,6 +1734,12 @@ public:
     else if (self.selectedAnnotation)
     {
         [self deselectAnnotation:self.selectedAnnotation animated:YES];
+        if ([self.delegate respondsToSelector:@selector(mapViewTapDidNotSelectAnnotation:)])
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.delegate mapViewTapDidNotSelectAnnotation:self];
+            });
+        }
     }
 }
 
@@ -2090,6 +2102,12 @@ public:
         {
             id<MGLAnnotation> annotation = [self annotationForGestureRecognizer:(UITapGestureRecognizer*)gestureRecognizer persistingResults:NO];
             if (!annotation) {
+                if ([self.delegate respondsToSelector:@selector(mapViewTapDidNotSelectAnnotation:)])
+                {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        [self.delegate mapViewTapDidNotSelectAnnotation:self];
+                    });
+                }
                 return NO;
             }
         }
