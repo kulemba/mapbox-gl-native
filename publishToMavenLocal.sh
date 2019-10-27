be998f9c374b243916104069ba87e2a3eb076074
Lukas Rössler <lukas@ulmon.com>
2019-10-27 09:19:07 +0100

    [android] various fixes: append ulmon postfix to version, add a maven publish script, ignore cxx folders
    (cherry picked from commit 9779bb42cb9dc101ae22e73e470fab4d9305de50)
    (cherry picked from commit f32b90fdf2736eb2b00dadef918c3f08f4142e35)

---
+++ publishToMavenLocal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pushd platform/android &> /dev/null
+./gradlew -Pmapbox.buildtype="release" -Pmapbox.abis="armeabi-v7a arm64-v8a x86 x86_64" :MapboxGLAndroidSDK:clean :MapboxGLAndroidSDK:publishMapboxMapsSdkPublicationPublicationToMavenLocal
+popd &> /dev/null
